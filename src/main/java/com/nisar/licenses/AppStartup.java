3df31846565ae3300c8425de363da609271fd875
Nisar-bhat <nisarshafi@gmail.com>
2022-07-05 20:17:27 +0530

    First commit

---
+++ src/main/java/com/nisar/licenses/AppStartup.java
@@ -0,0 +1,12 @@
+package com.nisar.licenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStartup {
+
+    public static void main(String [] args){
+        SpringApplication.run(AppStartup.class,args);
+    }
+}
